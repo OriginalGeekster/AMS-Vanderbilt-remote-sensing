ddff6e6fc7d68c3a56e2664e1c51a4d5921b66ed
OriginalGeekster <saskumar02@gmail.com>
2017-10-12 16:52:31 -0500

    Updated with comments explaining all code
    Added comments to explain and elucidate purpose and usage of functions.

--- AMS-final.ino
+++ AMS-final.ino
@@ -1,40 +1,34 @@
-#include <OneWire.h>
-#include <DallasTemperature.h>
-#include <LiquidCrystal.h>
-LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+#include <OneWire.h> //protocol to interface sensor w/ arduino
+#include <DallasTemperature.h> //does calculations for temp sensor from mV to degrees
+#include <LiquidCrystal.h> //control and interface library for LCD display
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2); //defines array of pins for LCD interface
 
-#define SensorPin 0          //pH meter Analog output to Arduino Analog Input 0
+#define SensorPin 0          //pH meter Analog output to Arduino Analog pin 0
 unsigned long int avgValue;  //Store the average value of the sensor feedback
 float b;
-int buf[10],temp;
-float pHval;
-// Data wire is plugged into pin 2 on the Arduino
-#define ONE_WIRE_BUS 7
+int buf[10],temp; //lists that store pH vaues while calculating
+float pHval; //final pH value that is transmitted to LCD Display
 
-// Setup a oneWire instance to communicate with any OneWire devices 
-// (not just Maxim/Dallas temperature ICs)
-OneWire oneWire(ONE_WIRE_BUS);
+#define ONE_WIRE_BUS 7 // data wire for temp sensor
+
+
+OneWire oneWire(ONE_WIRE_BUS); //oneWire protocol for temperature sensor
  
 // Pass our oneWire reference to Dallas Temperature.
 DallasTemperature sensors(&oneWire);
  
 void setup()
 {
-  // start serial port
-  lcd.begin(16,2);
+  lcd.begin(16,2); //define size of LCD
 
-  pinMode(13,OUTPUT);
-  lcd.print("Welcome to the AMO");
-  Serial.begin(300);
-  Serial.println("Dallas Temperature IC Control Library Demo");
+  pinMode(13,OUTPUT); // onBoard LED on arduino
+  lcd.print("Initializing..."); //Initializing message on LCD
   
-  // Start up the library
+  sensors.begin(); //begins requesting data from temp sensor
   
-  sensors.begin();
-  lcd.clear();
-  delay(300);
-  
-  lcd.setCursor(0,0);
+  delay(300); //display Initialize message for 300ms
+  lcd.clear(); //clear display
+  lcd.setCursor(0,0); //set print to first column, first row
 }
  
  
@@ -42,28 +36,28 @@ void loop()
 {
   
   
-  getPH();
-  Serial.print(" Requesting temperatures...");
-  sensors.requestTemperatures(); // Send the command to get temperatures
-  Serial.println("DONE");
-
-  Serial.print("Temperature is: ");
-  Serial.print(sensors.getTempFByIndex(0)); // Why "byIndex"?
-  lcd.setCursor(0,0);
-  lcd.print(sensors.getTempFByIndex(0));
+  getPH(); //pH calculation function, see below
+  lcd.setCursor(0,0); //reset display
+  /*
+  TEMPERATURE PRINT TO LCD
+  */ 
+  lcd.print(sensors.getTempFByIndex(0)); //print temp value from temp sensor
   lcd.setCursor(6,0);
-  lcd.print("F");
+  lcd.print("F"); //units
+  /*
+  pH PRINT TO LCD
+  */
   lcd.setCursor(0,1);
-  lcd.print(pHval);
+  lcd.print(pHval); //print pH val given by getPH function (see below)
   lcd.setCursor(6,1);
-  lcd.print("pH");
+  lcd.print("pH"); //units to do away with confusion
 }
 
 void getPH()
 {
   for(int i=0;i<10;i++)       //Get 10 sample value from the sensor for smooth the value
   { 
-    buf[i]=analogRead(SensorPin);
+    buf[i]=analogRead(SensorPin); //read values and print them to list
     
   }
   for(int i=0;i<9;i++)        //sort the analog from small to large
@@ -82,14 +76,9 @@ void getPH()
   for(int i=2;i<8;i++)                      //take the average value of 6 center sample
     avgValue+=buf[i];
   float phValue=(float)avgValue*5.0/1024/6; //convert the analog into millivolt
-  phValue=3.5*phValue; //convert the millivolt into pH value
-  pHval = phValue;
-  Serial.print("    pH:");  
-  Serial.print(phValue,2);
-  Serial.println(" ");
-  digitalWrite(13, HIGH);       
-  delay(800);
-  digitalWrite(13, LOW); 
+  phValue=3.5*phValue;                      //convert the millivolt into pH value
+  pHval = phValue;                          //set final pH value to float variable for printing to LCD
+   
 
 }
 
